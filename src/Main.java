f89034f85d0ad30c83be9b7f70f20156081dec7b
Luebu <maximilian.luecker@hebk.de>
2020-09-16 10:58:44 +0200

    initial

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,13 @@
 import java.util.Scanner;
-import java.util.Date;
+import java.lang.System;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Aufwand a = new Aufwand();
-        Date d = new Date();
 
         System.out.println("Wie oft Ente?");
-        double x = d.getTime();
+        double x = System.currentTimeMillis();
         a.ente(sc.nextInt());
-        System.out.println(x-d.getTime());
+        System.out.println(System.currentTimeMillis()-x);
     }
 }
