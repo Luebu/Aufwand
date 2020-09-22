de79ec6ed163a7076ca012fe9116bc8b12a345df
Luebu <maximilian.luecker@hebk.de>
2020-09-22 08:57:41 +0200

    Aufwand durch Anweisung, vergleich

--- src/Aufwand.java
+++ src/Aufwand.java
@@ -1,12 +1,25 @@
 import java.lang.System;
+
 public class Aufwand {
-    public void ente(int pX){
-        double x = System.currentTimeMillis();
-        for(int i = 0;i<pX;i++){
+
+    public void ente(int pX) {
+        int an = 3;
+        int ver = 0;
+        //double x = System.currentTimeMillis();
+        for (int i = 0; i < pX; i++) {
+
+            ver++;
             System.out.println("Ente");
+            an++;
+            an++;
         }
-        System.out.print("Es hat ");
+        ver++;
+        System.out.println(an);
+        System.out.println(ver);
+        /*System.out.print("Es hat ");
         System.out.print(System.currentTimeMillis() -x);
-        System.out.print(" Millisekunden gadauert!");
+        System.out.print(" Millisekunden gadauert!");*/
     }
 }
+//Anweisung = n*2+3
+//vergeleich = n+1
